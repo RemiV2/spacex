bbae6d9a5b5a83e2fac4c1c1f364d9d65ae291aa
Rémi de Juvigny <remidejuvigny@gmail.com>
2017-12-20 12:27:23 +0100

    started equipment slide

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,25 @@ console.log('js is ready')
 
 const scene = document.querySelector(".scene")
 const parallaxInstance = new Parallax(scene)
+const slides = Array.from(document.querySelectorAll('section.slide'))
+const buttons = Array.from(document.querySelectorAll('.button'))
+
+console.log(slides)
+console.log(buttons)
+
+let currentSlideIndex = 0
+
+const nextSlide = () => {
+  for (const slide of slides) {
+    slide.style.zIndex = '0'
+  }
+  slides[currentSlideIndex].style.zIndex = '100'
+  currentSlideIndex++
+}
+
+for (const button of buttons) {
+  button.addEventListener('click', nextSlide)
+}
+
+nextSlide()
+
