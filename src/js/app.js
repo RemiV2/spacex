1c2e1e8d25224d5536f16fd94b02ba8c1585d28e
Rémi de Juvigny <remidejuvigny@gmail.com>
2017-12-20 16:35:50 +0100

    control slides with keyboard

--- src/js/app.js
+++ src/js/app.js
@@ -8,15 +8,45 @@ for (const scene of scenes) {
   const parallaxInstance = new Parallax(scene)
 }
 
-let currentSlideIndex = 0
+let currentSlideIndex = -1
 
 const nextSlide = () => {
   for (const slide of slides) {
     slide.style.zIndex = '0'
   }
+  if (currentSlideIndex < slides.length-1) {
+    currentSlideIndex++
+  }
   slides[currentSlideIndex].style.zIndex = '100'
-  currentSlideIndex++
 }
+
+const previousSlide = () => {
+  for (const slide of slides) {
+    slide.style.zIndex = '0'
+  }
+  if (currentSlideIndex > 0) {
+    currentSlideIndex--
+  }
+  slides[currentSlideIndex].style.zIndex = '100'
+}
+
+document.addEventListener('keydown', (event) => {
+  console.log('keypress')
+  switch (event.keyCode) {
+    // Left arrow
+    case 37:
+      previousSlide()
+      break
+    // Right arrow
+    case 39:
+      nextSlide()
+      break
+    // Spacebar
+    case 32:
+      nextSlide()
+      break
+  }
+})
 
 for (const button of buttons) {
   button.addEventListener('click', nextSlide)
